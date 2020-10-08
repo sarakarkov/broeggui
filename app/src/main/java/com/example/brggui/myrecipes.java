f73ab5fb66d3d373acf0abd3126dce36863ef1e8
Sarakarkov <s195388@student.dtu.dk>
2020-10-08 16:58:03 +0200

    GUI fra Figma

---
+++ app/src/main/java/com/example/brggui/myrecipes.java
@@ -0,0 +1,14 @@
+package com.example.brggui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class myrecipes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_myrecipes);
+    }
+}
